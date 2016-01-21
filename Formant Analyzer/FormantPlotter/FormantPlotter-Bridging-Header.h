b2ce5551183b8288d3c6aed8fba1006f330fd24b
William Entriken <github.com@phor.net>
2016-01-21 16:44:24 -0500

    move main controller to swift

---
+++ Formant Analyzer/FormantPlotter/FormantPlotter-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "PlotView.h"
+#import <FSLineChart.h>
+#import <TOWebViewController.h>
+#import <FDSoundActivatedRecorder.h>
+#import <AudioToolbox/AudioToolbox.h>
+#import <AVFoundation/AVFoundation.h>
+#import <CoreMedia/CoreMedia.h>
+#import "SpeechAnalyzer.h"
