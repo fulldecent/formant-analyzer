5de4b80abcc4338ec0fad72343e14c6600a8456f
William Entriken <github.com@phor.net>
2025-10-14 17:17:41 -0700

    remove old

--- Offline MATLAB analysis/3. Vowel Isolation/isolateVowel.m
+++
@@ -1,7 +0,0 @@
-function [start, len] = isolateVowel(audioSamples)
-% ISOLATEVOWEL Analyzes an audio recording and finds the vowel sound
-
-    [strongStart, strongLen] = findStrongPartOfSignal(audioSamples);
-    fprintf('Strong part: %d ..< %d\n', strongStart, strongStart + strongLen);
-    [start, len] = truncateTailsOfRange(strongStart, strongLen);
-end
